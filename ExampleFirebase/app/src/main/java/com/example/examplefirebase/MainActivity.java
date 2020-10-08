77c74801c43276a50efe118d33c2705474c3f090
chaitanya <chaitanyakarri963@gmail.com>
2020-10-08 16:59:50 +0530

    08-10-2020

--- ExampleFirebase/app/src/main/java/com/example/examplefirebase/MainActivity.java
+++ ExampleFirebase/app/src/main/java/com/example/examplefirebase/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.examplefirebase;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -47,5 +51,47 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = LayoutInflater.from(this).inflate(R.layout.reset,null,false);
+        final EditText mail = v.findViewById(R.id.remail);
+        builder.setView(v);
+        builder.setCancelable(false);
+        builder.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String umail = mail.getText().toString();
+                if (umail.isEmpty()){
+                    Toast.makeText(MainActivity.this, "mail cant be empty",
+                            Toast.LENGTH_SHORT).show();
+                }
+                auth.sendPasswordResetEmail(umail).addOnCompleteListener(MainActivity.this,
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Mail sent",
+                                            Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(MainActivity.this, "failed to send", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 }
--- ExampleFirebase/app/src/main/java/com/example/examplefirebase/RegisterActivity.java
+++ ExampleFirebase/app/src/main/java/com/example/examplefirebase/RegisterActivity.java
@@ -1,18 +1,50 @@
 package com.example.examplefirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText mail,pass;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mail = findViewById(R.id.mail);
+        pass = findViewById(R.id.pass);
+        auth = FirebaseAuth.getInstance();
     }
 
     public void register(View view) {
+        String umail = mail.getText().toString();
+        String upass = pass.getText().toString();
+        if (umail.isEmpty() | upass.isEmpty()) {
+            Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show();
+        } else {
+            auth.createUserWithEmailAndPassword(umail,upass).addOnCompleteListener(this,
+                    new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
